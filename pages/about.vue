4885fd1287f0f54b92c3a3913c2589326ede8d8a
mana <mananico0319@gmail.comgit config --global user.email mananico0319@gmail.com>
2020-06-18 23:25:45 +0900

    add pages

---
+++ pages/about.vue
@@ -0,0 +1,663 @@
+<!-- HTML 　2ページ目-->
+
+<template>
+  <div>
+    <div class="container-fluid">
+    
+     <!-- ヘッダー -->
+      <div class="head-icon">
+        <div class="head-img">
+         <img src="~assets/images/lemon_logo.png" alt="レモン社ロゴ">
+        </div>
+      
+
+      <div class="d-none d-md-block">
+       <div class="navigation">
+       <nav>
+         <li>店舗情報</li>
+         <li>レモン社とは</li>
+         <li>商品アーカイブ</li>
+         <li>中古情報</li>
+         <li>商品レビュー</li>
+         <li>委託販売</li>
+         <li>商品を売る</li>
+       </nav>
+       </div>
+      </div>
+      </div>
+
+     <!-- レモン社とは -->
+      <div class="row">
+      <div class="col-sm-12">
+      <div class="about-lemon">
+        <div class="lemon-top">
+         <h2>レモン社とは</h2>
+        </div>
+       </div>
+      </div>
+      
+      <!--店舗コンセプト　spの時-->
+       <div class="col-sm-12 d-md-none">
+         <div class="concept">
+           <div class="concept-text">
+            <h2>店舗コンセプト</h2>
+           </div>
+         </div>
+       </div>
+
+       <div class="col-sm-12 d-md-none">
+         <div class="message">
+         <p><span>新しい何かに出会える<br>大人が来て見るだけでも楽しい<br>そんな店舗を目指しています。</span></p>
+         <p>東京・銀座で創業し33年。「大人の趣味の店」をコンセプトに、見て楽しい、さわって楽しい商材をそろえています。 ライカ、ハッセル等、高級輸入カメラにおいては、品揃え、価格ともにお客様のご希望にお応えできると自負しております。また、売ってお得、買ってお得な「レモンの委託販売」ではカメラはもちろん、時計、万年筆、鉄道模型、ミニカーも好評です。</p>
+         </div>
+       </div>
+
+       <div class="col-sm-12 d-md-none ">
+         <div class="kodawari">
+          <div class="kodawari-text">
+          <h2>こだわり</h2>
+          </div>
+         </div>
+       </div>
+       <div class="col-sm-12 d-md-none ">
+         <div class="message">
+          <p><span>たとえ不格好でも<br>こだわりがあればいい…。<br>そんな商品をもっと知って欲しい。<br>レモン社独自のセレクトショップです。</span></p>
+          <p>例えばカメラ。何でも機能がついている、キレイに撮影できるという魅力より、カメラがカメラらしさを放つのは、精密機械としての加工の細かさであり、美しさであったり、持ったときの重量感、作るまでのストーリーや時代背景、精密的な機械の美しさが心を引きつけるからです。</p>
+         </div>
+       <hr>
+       </div>
+
+       <!--店舗コンセプト　md以上-->
+       　<div class="d-none d-md-block col-md-6">
+         <div class="concept2">
+           <div class="concept-text2">
+            <h2>店舗コンセプト</h2>
+           </div>
+         </div>
+       </div>
+
+       <div class="d-none d-md-block col-md-6">
+         <div class="message2">
+         <p><span>新しい何かに出会える<br>大人が来て見るだけでも楽しい<br>そんな店舗を目指しています。</span></p>
+         <p>東京・銀座で創業し33年。「大人の趣味の店」をコンセプトに、見て楽しい、さわって楽しい商材をそろえています。 ライカ、ハッセル等、高級輸入カメラにおいては、品揃え、価格ともにお客様のご希望にお応えできると自負しております。また、売ってお得、買ってお得な「レモンの委託販売」ではカメラはもちろん、時計、万年筆、鉄道模型、ミニカーも好評です。</p>
+         </div>
+       </div>
+
+
+         
+       <div class="d-none d-md-block col-md-6">
+         <div class="message2">
+          <p><span>たとえ不格好でも<br>こだわりがあればいい…。<br>そんな商品をもっと知って欲しい。<br>レモン社独自のセレクトショップです。</span></p>
+          <p>例えばカメラ。何でも機能がついている、キレイに撮影できるという魅力より、カメラがカメラらしさを放つのは、精密機械としての加工の細かさであり、美しさであったり、持ったときの重量感、作るまでのストーリーや時代背景、精密的な機械の美しさが心を引きつけるからです。</p>
+         </div>
+       </div>
+
+       <div class="d-none d-md-block col-md-6">
+         <div class="kodawari2">
+          <div class="kodawari-text2">
+          <h2>こだわり</h2>
+          </div>
+         </div>
+       </div>
+      </div>
+      <hr>
+  
+    <!-- 会社概要　-->
+    <div class="company">
+      <div class="row">
+        <div class="d-md-none">
+        <div class="company-text">
+         <h2>会社概要</h2>
+        </div>
+     
+      <table width="100%">
+        <tr><th width="30%">商号</th><td width="70%">ナニワグループ(全5社)</td></tr>
+        <tr><th>会社名</th><td>株式会社レモン社</td></tr>
+        <tr><th>本社</th><td>大阪市中央区心斎橋筋1丁目3番12号</td></tr>
+        <tr><th>代表者</th><td>代表取締役社長　塩山　知之</td></tr>
+        <tr><th>創業</th><td>1983年11月</td></tr>
+        <tr><th>事業内容</th><td>カメラ・レンズ新品中古小売販売業<br>時計・万年筆等小売販売業<br>証明写真スタジオ事業</td></tr>
+      </table>
+        </div>
+      </div>
+    </div>
+     
+    <div class="naniwa-group">
+      <div class="row">
+        <div class="d-md-none">
+     <div class="naniwa">
+      <h3>ナニワグループ</h3>
+     </div>
+
+      <table width="100%">
+        <tr><th width="30%">創業</th><td width="70%">1947年4月1日</td></tr>
+        <tr><th>設立</th><td>1951年9月14日</td></tr>
+        <tr><th>総資本金</th><td>1億4,432万円(2020年4月1日現在)</td></tr>
+        <tr><th>グループ代表</th><td>塩山　知之</td></tr>
+        <tr><th>事業内容</th><td>写真機器、写真材料、家電製品、OA機器、時計などの小売及び卸業、写真現像プリント加工、デジタル画像、デジタルアウトプットサービス、スタジオ撮影</td></tr>
+        <tr><th>総従業員数</th><td>255名(海外60名)2020年4月1日現在</td></tr>
+        <tr><th>本社</th><td>〒530-0012　大阪市北区芝田2-1-18 西阪急ビル6F</td></tr>
+        <tr><th>ホームページ</th><td>https://www.cameranonaniwa.co.jp/</td></tr>
+        <tr><th>ナニワグループ各社</th><td><ol><li>(株)ナニワ商会</li><li>(株)フジカラー新日本</li><li>(株)レモン社</li><li>(株)フォトバンク</li><li>(株)フォトバンク　ベトナム</li></ol></td></tr>
+      </table>
+
+        </div>
+      </div>
+    </div>
+    
+   <!--　会社概要　md以上　-->
+    <div class="company2">
+      <div class="row">
+
+        <div class="d-none d-md-block">
+        <div class="company-text">
+         <h2>会社概要</h2>
+        </div>
+     
+      <table width="100%">
+        <tr><th width="30%">商号</th><td width="70%">ナニワグループ(全5社)</td></tr>
+        <tr><th>会社名</th><td>株式会社レモン社</td></tr>
+        <tr><th>本社</th><td>大阪市中央区心斎橋筋1丁目3番12号</td></tr>
+        <tr><th>代表者</th><td>代表取締役社長　塩山　知之</td></tr>
+        <tr><th>創業</th><td>1983年11月</td></tr>
+        <tr><th>事業内容</th><td>カメラ・レンズ新品中古小売販売業<br>時計・万年筆等小売販売業<br>証明写真スタジオ事業</td></tr>
+      </table>
+        </div>
+      </div>
+    </div>
+     
+    <div class="naniwa-group2">
+      <div class="row">
+
+        <div class="d-none d-md-block">
+     <div class="naniwa">
+      <h3>ナニワグループ</h3>
+     </div>
+
+      <table width="100%">
+        <tr><th width="30%">創業</th><td width="70%">1947年4月1日</td></tr>
+        <tr><th>設立</th><td>1951年9月14日</td></tr>
+        <tr><th>総資本金</th><td>1億4,432万円(2020年4月1日現在)</td></tr>
+        <tr><th>グループ代表</th><td>塩山　知之</td></tr>
+        <tr><th>事業内容</th><td>写真機器、写真材料、家電製品、OA機器、時計などの小売及び卸業、写真現像プリント加工、デジタル画像、デジタルアウトプットサービス、スタジオ撮影</td></tr>
+        <tr><th>総従業員数</th><td>255名(海外60名)2020年4月1日現在</td></tr>
+        <tr><th>本社</th><td>〒530-0012　大阪市北区芝田2-1-18 西阪急ビル6F</td></tr>
+        <tr><th>ホームページ</th><td>https://www.cameranonaniwa.co.jp/</td></tr>
+        <tr><th>ナニワグループ各社</th><td><ol><li>(株)ナニワ商会</li><li>(株)フジカラー新日本</li><li>(株)レモン社</li><li>(株)フォトバンク</li><li>(株)フォトバンク　ベトナム</li></ol></td></tr>
+      </table>
+
+        </div>
+      </div>
+    </div>
+    
+
+    <!-- 沿革 -->
+    <div class="row">
+     <div class="enkaku">
+      <div class="d-md-none">
+        <div class="title">
+        <h3>沿革</h3>
+        </div>
+      </div>
+      <div class="col-md-6 d-md-none">
+        <div class="date">
+        <p>1983年11月</p>
+        </div>
+        <hr>
+      </div>
+      <div class="col-md-6 d-md-none">
+        <div class="place">
+        <p>東京都中央区銀座4丁目<br>吉田ビルにて開始</p>
+        </div>
+        <div class="date">
+        <p>1994年6月</p>
+        <hr>
+        </div>
+      </div>
+      <div class="col-md-6 d-md-none">
+        <div class="place">
+        <p>東京都中央区銀座4丁目<br>西銀座ビル増床</p>
+        </div>
+        <div class="date">
+        <p>1994年2月</p>
+        <hr>
+        </div>
+      </div>
+      <div class="col-md-6 d-md-none">
+        <div class="place">
+        <p>日本初の正規ライカ特約店契約</p>
+        </div>
+        <div class="date">
+        <p>1997年10月</p>
+        <hr>
+        </div>
+      </div>
+      <div class="col-md-6 d-md-none">
+        <div class="place">
+        <p>大阪店　大阪駅前第3ビルへ移転<br>(西区靭本町より)</p>
+        </div>
+        <div class="date">
+        <p>1999年12月</p>
+        <hr>
+        </div>
+      </div>
+      <div class="col-md-6 d-md-none">
+        <div class="place">
+        <p>レモン・メンバーズカード開始</p>
+        </div>
+        <div class="date">
+        <p>2000年5月</p>
+        <hr>
+        </div>
+      </div>
+      <div class="col-md-6 d-md-none">
+        <div class="place">
+        <p>正規ライカ特約店契約解約</p>
+        </div>
+        <div class="date">
+        <p>2000年11月</p>
+        <hr>
+        </div>
+      </div>
+      <div class="col-md-6 d-md-none">
+        <div class="place">
+        <p>大阪店 堂島アバンザビルへ移転<br>（大阪駅前第3ビルより）</p>
+        </div>
+        <div class="date">
+        <p>2001年7月</p>
+        <hr>
+        </div>
+      </div>
+      <div class="col-md-6 d-md-none">
+        <div class="place">
+        <p>銀座店 レンタルラボ スペース開設</p>
+        </div>
+        <div class="date">
+        <p>2001年4月</p>
+        <hr>
+        </div>
+      </div>
+      <div class="col-md-6 d-md-none">
+        <div class="place">
+        <p>福岡店 福岡天神駅前にオープン</p>
+        </div>
+        <div class="date">
+        <p>2001年12月</p>
+        <hr>
+        </div>
+      </div>
+      <div class="col-md-6 d-md-none">
+        <div class="place">
+        <p>ブランドショップ・レモネード 銀座7丁目にオープン</p>
+        </div>
+        <div class="date">
+        <p>2002年4月</p>
+        <hr>
+        </div>
+      </div>
+      <div class="col-md-6 d-md-none">
+        <div class="place">
+        <p>大阪店 大阪駅前第3ビルへ移転<br>(堂島アバンザビルより）</p>
+        </div>
+        <div class="date">
+        <p>2003年5月</p>
+        <hr>
+        </div>
+      </div>
+      <div class="col-md-6 d-md-none">
+        <div class="place">
+        <p>創立20周年キャンペーン開始</p>
+        </div>
+        <div class="date">
+        <p>2005年1月</p>
+        <hr>
+        </div>
+      </div>
+      <div class="col-md-6 d-md-none">
+        <div class="place">
+        <p>西銀座ビル（並木通り）から<br>（現）銀座教会堂ビルへ統合移転</p>
+        </div>
+        <div class="date">
+        <p>2010年7月</p>
+        <hr>
+        </div>
+      </div>
+      <div class="col-md-6 d-md-none">
+        <div class="place">
+        <p>プリント、鉄道模型/琉映ビルから<br>（現）銀座教会堂ビルへ統合</p>
+        </div>
+        <div class="date">
+        <p>2010年7月</p>
+        <hr>
+        </div>
+      </div>
+      <div class="col-md-6 d-md-none">
+        <div class="place">
+        <p><br>（現）銀座教会堂ビルへ統合移転</p>
+        </div>
+        <div class="date">
+        <p>2010年7月</p>
+        <hr>
+        </div>
+      </div>
+      <div class="col-md-6 d-md-none">
+        <div class="place">
+        <p>新宿店　西新宿にオープン</p>
+        </div>
+        <div class="date">
+        <p>2013年7月</p>
+        <hr>
+        </div>
+      </div>
+      <div class="col-md-6 d-md-none">
+        <div class="place">
+        <p>銀座店 写真スタジオ「スタジオ728」開設</p>
+        </div>
+        <div class="date">
+        <p>2014年3月</p>
+        <hr>
+        </div>
+      </div>
+      <div class="col-md-6 d-md-none">
+        <div class="place">
+        <p>日本橋三越店<br>三越日本橋本店7階にオープン</p>
+        </div>
+        <div class="date">
+        <p>2015年8月</p>
+        <hr>
+        </div>
+      </div>
+      <div class="col-md-6 d-md-none">
+        <div class="place">
+        <p>秋葉原店　秋葉原駅前にオープン<br>名古屋栄店　名古屋松坂屋店そばにオープン</p>
+        </div>
+        <div class="date">
+        <p>2019年3月</p>
+        <hr>
+        </div>
+      </div>
+      <div class="col-md-6 d-md-none">
+        <div class="place">
+        <p>横浜店　横浜駅西口にオープン</p>
+        </div>
+        <div class="date">
+        <p>2019年9月</p>
+        <hr>
+        </div>
+      </div>
+      <div class="col-md-6 d-md-none">
+        <div class="place-last">
+        <p><span>池袋店 JR池袋駅東口にオープン</span></p>
+        <hr>
+        </div>
+      </div>
+     </div>
+    </div>
+
+    <!-- 沿革　md以上　-->
+    <div class="enkaku2">
+     <div class="row">
+      <div class="d-none d-md-block col-md-12">
+       <div class="title2">
+        <h3>沿革</h3>
+       </div>
+      </div>
+      <div class="d-none d-md-block col-md-6">
+        <div class="date-right">
+        <p>1983年11月</p>
+        </div>
+      </div>
+      <div class="d-none d-md-block col-md-6">
+        <div class="enkaku-text">
+        <p></p>
+        </div>
+      </div>
+      <div class="d-none d-md-block col-md-6">
+        <div class="enkaku-text">
+        <p>東京都中央区銀座4丁目<br>吉田ビルにて開始</p>
+        </div>
+      </div>
+      <div class="d-none d-md-block col-md-6">
+        <div class="date-left">
+         <p>1994年6月</p>
+        </div>
+      </div>
+      <div class="d-none d-md-block col-md-6">
+        <div class="date-right">
+        <p>1996年2月</p>
+        </div>
+      </div>
+      <div class="d-none d-md-block col-md-6">
+        <div class="enkaku-text">
+        <p>東京都中央区銀座4丁目<br>西銀座ビル増床</p>
+        </div>
+      </div>
+      <div class="d-none d-md-block col-md-6">
+        <div class="enkaku-text">
+        <p>日本初の正規ライカ特約店契約</p>
+        </div>
+      </div>
+      <div class="d-none d-md-block col-md-6">
+        <div class="date-left">
+        <p>1997年10月</p>
+        </div>
+      </div>
+      <div class="d-none d-md-block col-md-6">
+        <div class="date-right">
+         <p>1999年12月</p>
+        </div>
+      </div>
+      <div class="d-none d-md-block col-md-6">
+        <div class="enkaku-text">
+        <p>大阪店 大阪駅前第3ビルへ移転<br>（西区靭本町より）</p>
+        </div>
+      </div>
+      <div class="d-none d-md-block col-md-6">
+        <div class="enkaku-text">
+        <p>レモン・メンバーズカード開始</p>
+        </div>
+      </div>
+      <div class="d-none d-md-block col-md-6">
+        <div class="date-left">
+        <p>2000年5月</p>
+        </div>
+      </div>
+      <div class="d-none d-md-block col-md-6">
+        <div class="date-right">
+        <p>2000年11月</p>
+        </div>
+      </div>
+      <div class="d-none d-md-block col-md-6">
+        <div class="enkaku-text">
+        <p>正規ライカ特約店契約解約</p>
+        </div>
+      </div>
+      <div class="d-none d-md-block col-md-6">
+        <div class="enkaku-text">
+        <p>大阪店 堂島アバンザビルへ移転<br>（大阪駅前第3ビルより）</p>
+        </div>
+      </div>
+      <div class="d-none d-md-block col-md-6">
+        <div class="date-left">
+        <p>2001年7月</p>
+        </div>
+      </div>
+      <div class="d-none d-md-block col-md-6">
+        <div class="date-right">
+        <p>2001年4月</p>
+        </div>
+      </div>
+      <div class="d-none d-md-block col-md-6">
+        <div class="enkaku-text">
+        <p>銀座店 レンタルラボ スペース開設</p>
+        </div>
+      </div>
+      <div class="d-none d-md-block col-md-6">
+        <div class="enkaku-text">
+        <p>福岡店 福岡天神駅前にオープン</p>
+        </div>
+      </div>
+      <div class="d-none d-md-block col-md-6">
+        <div class="date-left">
+        <p>2001年12月</p>
+        </div>
+      </div>
+      <div class="d-none d-md-block col-md-6">
+        <div class="date-right">
+        <p>2002年4月</p>
+        </div>
+      </div>
+      <div class="d-none d-md-block col-md-6">
+        <div class="enkaku-text">
+        <p>ブランドショップ・レモネード 銀座7丁目にオープン</p>
+        </div>
+      </div>
+      <div class="d-none d-md-block col-md-6">
+        <div class="enkaku-text">
+        <p>大阪店 大阪駅前第3ビルへ移転<br>（堂島アバンザビルより）</p>
+        </div>
+      </div>
+      <div class="d-none d-md-block col-md-6">
+        <div class="date-left">
+        <p>2003年5月</p>
+        </div>
+      </div>
+      <div class="d-none d-md-block col-md-6">
+        <div class="date-right">
+        <p>2005年1月</p>
+        </div>
+      </div>
+      <div class="d-none d-md-block col-md-6">
+        <div class="enkaku-text">
+        <p>創立20周年キャンペーン開始</p>
+        </div>
+      </div>
+      <div class="d-none d-md-block col-md-6">
+        <div class="enkaku-text">
+        <p>西銀座ビル（並木通り）から<br>現）銀座教会堂ビル8階へ拡張移転</p>
+        </div>
+      </div>
+      <div class="d-none d-md-block col-md-6">
+        <div class="date-left">
+        <p>2007年11月</p>
+        </div>
+      </div>
+      <div class="d-none d-md-block col-md-6">
+        <div class="date-right">
+        <p>2010年7月</p>
+        </div>
+      </div>
+      <div class="d-none d-md-block col-md-6">
+        <div class="enkaku-text">
+        <p>銀座本店（吉田ビル/並木通り）から<br>現）銀座教会堂ビルへ統合移転</p>
+        </div>
+      </div>
+      <div class="d-none d-md-block col-md-6">
+        <div class="enkaku-text">
+        <p>プリント、鉄道模型/琉映ビルから<br>銀座教会堂ビルへ統合</p>
+        </div>
+      </div>
+      <div class="d-none d-md-block col-md-6">
+        <div class="date-left">
+        <p>2011年4月</p>
+        </div>
+      </div>
+      <div class="d-none d-md-block col-md-6">
+        <div class="date-right">
+        <p>2013年7月</p>
+        </div>
+      </div>
+      <div class="d-none d-md-block col-md-6">
+        <div class="enkaku-text">
+        <p>新宿店 西新宿にオープン</p>
+        </div>
+      </div>
+      <div class="d-none d-md-block col-md-6">
+        <div class="enkaku-text">
+        <p>銀座店 写真スタジオ「スタジオ728」開設</p>
+        </div>
+      </div>
+      <div class="d-none d-md-block col-md-6">
+        <div class="date-left">
+        <p>2014年3月</p>
+        </div>
+      </div>
+      <div class="d-none d-md-block col-md-6">
+        <div class="date-right">
+        <p>2015年8月</p>
+        </div>
+      </div>
+      <div class="d-none d-md-block col-md-6">
+        <div class="enkaku-text">
+        <p>日本橋三越店<br>三越日本橋本店7階にオープン</p>
+        </div>
+      </div>
+      <div class="d-none d-md-block col-md-6">
+        <div class="enkaku-text">
+        <p>秋葉原店 秋葉原駅前にオープン<br>名古屋栄店 名古屋松坂屋店そばにオープン</p>
+        </div>
+      </div>
+      <div class="d-none d-md-block col-md-6">
+        <div class="date-left">
+        <p>2019年3月</p>
+        </div>
+      </div>
+      <div class="d-none d-md-block col-md-6">
+        <div class="date-right">
+        <p>2019年9月</p>
+        </div>
+      </div>
+      <div class="d-none d-md-block col-md-6">
+        <div class="enkaku-text">
+        <p>横浜店 横浜駅西口にオープン</p>
+        </div>
+      </div>
+      <div class="d-none d-md-block col-md-6">
+        <div class="enkaku-text">
+        <p>池袋店 JR池袋駅東口にオープン</p>
+        </div>
+      </div>
+
+      <hr>
+
+    </div>
+    </div>
+    <!-- ブログ -->
+    <div class="blog">
+
+      <div class="row">
+
+       <div class="blog-naniwa">
+        <div class="col-sm-12 col-md-6">
+          <img src="~assets/images/bnr_naniwa.jpg" alt="ナニワグループオンライン">
+        </div>
+       </div>
+
+       <div class="blog-lemon">
+         <div class="col-sm-12 col-md-6">
+          <img src="~assets/images/bnr_lemonblog.jpg" alt="レモンブログ">
+         </div>
+        </div>
+      </div>
+
+    </div>
+    
+
+
+    <!-- フッター -->
+    </div>
+  </div>
+</template>
+
+<!-- JavaScript -->
+<script>
+
+export default {
+  
+}
+</script>
+
+<!-- css -->
+<style lang="scss" scoped>
+@import"~assets/scss/about.scss"
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,304 @@
+<!-- HTML 　1ページ目-->
+
+<template>
+  <div>
+    <div class="container">
+
+      <div class="top-page">
+        
+
+       <!-- MENU -->
+        <div class="head-icon">
+          <div>
+            <img src="~assets/images/lemon_logo.png" alt="レモン社ロゴ" width="95px" height="30px">
+          </div>
+
+          <!-- md以上で表示 -->
+          <div class="d-none d-md-block　#EE5635">
+            <p>MENU</p>
+          </div>
+          
+        </div>
+
+       <!-- 酸いも甘いも… -->
+        <div class="texts">
+            <div id="top-text">
+              <p>酸いも甘いも</p>
+              <p>知り尽くした</p>
+              <p>大人が集まる</p>
+              <p>趣味のお店</p>
+            </div>
+            </div>
+      </div>
+
+      <!-- NEWS -->
+      <div class="news">
+        <div class="row">
+          <div class="col-sm-6 col-md-2">
+             <p>NEWS</p>
+          </div>
+          <div class="col-sm-12 col-md-10">
+          　<p>2020.4.15</p>
+            <p>レモン社銀座・新宿・秋葉原・横浜・池袋は臨時休業中です。</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="information">
+       <div class="row">
+      <div class="col-sm-12 col-md-6 col-lg-4">
+        <div class="shop">
+          <div class="shop-text">
+            <h2>店舗情報</h2>
+            <hr>
+            <p>東京を中心に全国6店舗を展開する、「大人の趣味の店」レモン社。なかなか普段お目にかかることのできないような代物を揃え、見に来るだけでも楽しい店舗を目指しています。
+            </p>
+          </div>
+          </div>
+        </div>
+
+        <div class="col-sm-12 col-md-6 col-lg-4">
+         <div class="lemon">
+           <div class="lemon-text">
+            <h2>レモン社とは</h2>
+            <hr>
+            <p>新しい何かに出会える、大人が来て見るだけでも楽しいそんな店舗を目指しています。東京・銀座で創業し33年。「大人の趣味の店」をコンセプトに、見て楽しい、さわって楽しい商材をそろえています。
+            </p>
+           </div>
+          </div>
+        </div>
+
+        <div class="col-sm-12 col-md-6 col-lg-4">
+         <div class="sell">
+           <div class="sell-text">
+            <h2>商品を売る</h2>
+            <hr>
+            <p>ナニワグループ70年の伝統だからできる安心のカメラ買取・下取り。当社には胸を張って送り出すことのできる、ベテランの専門スタッフが多数在籍しております。見積査定は無料。店頭買取はもちろん、出張買取も承ります。</p>
+            <p><span>カメラのナニワ買取センター</span></p>
+           </div>
+          </div>
+        </div>
+
+        <div class="col-sm-12 col-md-6 col-lg-3 ">
+          <div class="buy">
+            <div class="buy-text">
+            <h2>商品を買う</h2>
+            <hr>
+            <p>レモン社の商品は、ナニワグループオンライン購入することが可能です。カメラ・レンズが様々な条件で検索ができ、全国33店舗のグループ店舗から取り寄せ可能です。あなたのカメラ選びをサポートします！</p>
+            <p><span>ナニワグループオンライン</span></p>
+            </div>
+          </div>
+        </div>
+
+          <div class="col-sm-12 col-md-6 col-lg-3">
+           <div class="old">
+             <div class="old-text">
+            <h2>中古情報</h2>
+            <hr>
+            <p>レモン社各店舗で入荷した商品を厳選して商品の性能や特徴などをわかりやすく掲載しています。中古品の多くは、カタログや資料が無くなってしまっているので商品探しの参考になるように努めています。</p>
+           </div>
+           </div>
+          </div>
+
+          <div class="col-sm-12 col-md-6 col-lg-3">
+           <div class="review">
+             <div class="review-text">
+            <h2>商品レビュー</h2>
+            <hr>
+            <p>レモン社社員・スタッフが気になる商品を実際に撮影した作例とレビューをわかりやすく掲載しています。最新のデジタル一眼からオールドレンズまで様々な状況で実写した結果をもとに使用感をまとめています。
+            </p>
+            </div>
+           </div>
+          </div>
+
+          <div class="col-sm-12 col-md-6 col-lg-3">
+            <div class="offer">
+             <div class="offer-text">
+            <h2>委託販売</h2>
+            <hr>
+            <p>お客様からお預かりしたカメラ・レンズを専門知識が豊富な社員・スタッフが店頭やWEBで代理販売いたします。最新の中古市場価格と全国33店舗のナニワグループの販売実績から適正な販売価格をご提案します。
+            </p>
+            </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+      
+    　<!-- 各店舗地図　-->
+    　<div class="shops">
+      <div class="row">
+        <div class="col-sm-12 col-md-6 name">
+          <h2>銀座教会店</h2>
+        </div>
+        <div class="col-sm-12 col-md-6">
+          <table>
+            <tbody>
+            <tr>
+             <th>所在地</th>
+             <td>〒104-0061<br>東京都中央区銀座4-2-1<br>銀座教会堂ビル8F</td>
+            </tr>
+            <tr>
+             <th>電話番号</th>
+             <td>03-3567-3131</td>
+            </tr>
+            <tr>
+             <th>営業時間</th>
+             <td>AM11:00～PM8:00<br>(日曜・祝祭日 AM11:00~PM7:00)</td>
+            </tr>
+            </tbody>
+          </table>
+        </div>
+        
+
+
+        <div class="col-sm-12 col-md-6">
+          <h2>新宿店</h2>
+        </div>
+        <div class="col-sm-12 col-md-6">
+         <table>
+          <tbody>
+          <tr>
+            <th>所在地</th>
+            <td>〒160-0023<br>東京都新宿区西新宿1-15-4<br>第2セイコービル3F</td>
+          </tr>
+          <tr>
+            <th>電話番号</th>
+            <td>03-5909-2333</td>
+          </tr>
+          <tr>
+            <th>営業時間</th>
+            <td>AM10:30～PM8:00</td>
+          </tr>
+          </tbody>
+         </table>
+        </div>
+
+      <div class="col-sm-12 col-md-6">
+          <h2>秋葉原店</h2>
+        </div>
+        <div class="col-sm-12 col-md-6">
+         <table>
+          <tbody>
+          <tr>
+            <th>所在地</th>
+            <td>〒101-0025<br>東京都千代田区神田佐久間町1-19</td>
+          </tr>
+          <tr>
+            <th>電話番号</th>
+            <td>03-3253-0255</td>
+          </tr>
+          <tr>
+            <th>営業時間</th>
+            <td>AM10:30～PM7:30</td>
+          </tr>
+          </tbody>
+         </table>
+        </div>
+
+        <div class="col-sm-12 col-md-6">
+          <h2>池袋店</h2>
+        </div>
+        <div class="col-sm-12 col-md-6">
+         <table>
+          <tbody>
+          <tr>
+            <th>所在地</th>
+            <td>〒170-0013<br>東京都豊島区東池袋1-9-5 清水ビル3階</td>
+          </tr>
+          <tr>
+            <th>電話番号</th>
+            <td>03-6709-2120</td>
+          </tr>
+          <tr>
+            <th>営業時間</th>
+            <td>AM10:30～PM7:30</td>
+          </tr>
+          </tbody>
+         </table>
+        </div>
+
+        <div class="col-sm-12 col-md-6">
+          <h2>横浜店</h2>
+        </div>
+        <div class="col-sm-12 col-md-6">
+         <table>
+          <tbody>
+          <tr>
+            <th>所在地</th>
+            <td>〒220-0005<br>横浜市西区南幸町2丁目17番19号<br>島田ビル1F</td>
+          </tr>
+          <tr>
+            <th>電話番号</th>
+            <td>045-620-7023</td>
+          </tr>
+          <tr>
+            <th>営業時間</th>
+            <td>AM10:30～PM7:30</td>
+          </tr>
+          </tbody>
+         </table>
+        </div>
+
+        <div class="col-sm-12 col-md-6">
+          <h2>名古屋栄店</h2>
+        </div>
+        <div class="col-sm-12 col-md-6">
+         <table>
+          <tbody>
+          <tr>
+            <th>所在地</th>
+            <td>〒460-0008<br>愛知県名古屋市中区栄3-30-28<br>岩田サカエビル　4F</td>
+          </tr>
+          <tr>
+            <th>電話番号</th>
+            <td>052-262-5557</td>
+          </tr>
+          <tr>
+            <th>営業時間</th>
+            <td>AM10:30～PM8:00<br>(日曜・祝祭日 AM10:30～PM7:00)</td>
+          </tr>
+          </tbody>
+         </table>
+        </div>
+
+      </div>
+     </div>
+
+    <!-- Facebook　Twitter-->
+    <div class="account">
+      <div class="row">
+      <div class="col-sm-12 col-md-6">
+        <h3><i class="fab fa-facebook-f fa-lg fa-fw my-small"></i>Facebook</h3>
+        <div class="fb-page" data-href="https://www.facebook.com/lemonsha.ginza/" data-tabs="timeline" data-width="500" data-height="400" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/lemonsha.ginza/" class="fb-xfbml-parse-ignore">
+          <a href="https://www.facebook.com/lemonsha.ginza/">レモン社</a></blockquote>
+        </div>
+      </div>
+      <div class="col-sm-12 col-md-6">
+        <h3><i class="fab fa-twitter fa-lg fa-fw my-small"></i>Twitter</h3>
+        <div class="twitter">
+          <a class="twitter-timeline" data-width="500" data-height="400" data-chrome="noheader nofooter" href="https://twitter.com/lemonsha?ref_src=twsrc%5Etfw"></a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+        </div>
+      </div>
+      </div>
+    </div>
+
+<div id="fb-root"></div>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/ja_JP/sdk.js#xfbml=1&version=v7.0"></script>
+
+    </div>
+  </div>
+</template>
+
+<!-- JavaScript -->
+<script>
+
+export default {
+  
+}
+</script>
+
+<!-- css -->
+<style>
+
+</style>
